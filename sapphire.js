f7a2e2b4c98b691d04957bb809db561e53e54471
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-06-17 07:56:19 -0400

    Create sapphire.js

---
+++ sapphire.js
@@ -0,0 +1,10 @@
+if(typeof jQuery === undefined){
+  throw "jQuery is required for sapphire to work. Didn't you read the README?";
+}
+(function ( $ ) {
+ 
+    $.fn.sapphire = function() {
+        
+    };
+ 
+}( jQuery ));
