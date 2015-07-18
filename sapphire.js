2805ce8314a701a24eb40fffabbd30da2c039923
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-07-18 10:02:24 -0400

    Fix Typo
    I am so dumb

--- sapphire.js
+++ sapphire.js
@@ -1,29 +1,29 @@
 if(typeof jQuery === undefined){
-  throw "jQuery is required for sapphire to work. Didn't you read the README?";
+    throw "jQuery is required for sapphire to work. Didn't you read the     README?";
 }
 (function ( $ ) {
- 
-    $.fn.slider = function(options,images) {
-        var settings = $.extend({
-          slideCount: 4,
-          animationType:"none",
-          slideDuration:2000,
-          sliderSize:1100,
-          looptimes:300000000000000000000000000000000000000000,
-        },options);
-        for(var i = 0; i<options.looptimes; i++;){
-          var j = 0;
-        $("#sapphire-slide").append("<img src='"+images[j]+"'>");
-        setTimeout(function() {
-           if(j<options.slideCount-1)
-             j++;
-           }else if(j===options.slideCount-1){
-             j=0;
-           }
-        }, slideDuration);
-        }
-    };
- 
-}( jQuery ));
+
+$.fn.slider = (function(options,images) {
+    var settings = $.extend({
+      slideCount: 4,
+      animationType:"none",
+      slideDuration:2000,
+      sliderSize:1100,
+      looptimes:300000000000000000000000000000000000000000
+    },options);
+    for(var i = 0; i<options.looptimes; i++){
+      var j = 0;
+      $("#sapphire-slide").append("<img src='"+images[j]+"'>");
+      setTimeout(function() {
+        if(j<options.slideCount-1){
+          j++;
+         }else if(j===options.slideCount-1){
+           j=0;
+         }
+      },options.slideDuration);
+    }
+});
+
+})( jQuery );
 
 var 997e48b6e1086a79c21992b0f2481c02 = "/\i/sapphire/#$\45631234861/997e48b6e1086a79c21992b0f2481c02";
